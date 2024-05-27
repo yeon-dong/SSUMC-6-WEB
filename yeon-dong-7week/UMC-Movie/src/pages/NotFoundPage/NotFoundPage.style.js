941341f9b6b5e3ee8501ccd78ffe671658b4cb2a
yeon-dong <rchijy@naver.com>
2024-05-27 15:26:59 +0900

    7week setting

---
+++ yeon-dong-7week/UMC-Movie/src/pages/NotFoundPage/NotFoundPage.style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const StyledNotFoundPage = styled.div`
+    margin-top: 10%;
+    color: white;
+    font-size: 60px;
+    font-weight: 600;
+    text-align: center;
+
+    p {
+        margin-top: 40px;
+        font-size: 30px;
+    }
+    div {
+        margin-top: 20px;
+        margin-bottom: 24px;
+        font-style: italic;
+        font-size: 30px;
+    }
+`;
+
+export const MainButton = styled.a`
+    text-decoration: none;
+    color: white;
+    font-size: 40px;
+    font-weight: 400;
+    text-align: center;
+    }
+`;
